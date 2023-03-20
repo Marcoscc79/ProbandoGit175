39c66255f9ac511159d7ee5d8518e94f66414343
Marcos Cielo <macccielo27879@gmail.com>
2023-03-20 11:12:16 -0300

    Primer commit de prueba

---
+++ app.js
@@ -0,0 +1,23 @@
+var auto = "Corolla"
+console.log(auto)
+var auto2 = 308
+console.log(auto2)
+var auto3= "C4"
+console.log(auto3)
+
+var nombre = "Marcos"
+var apellido = "Cielo"
+console.log(nombre, apellido)
+
+var fruta = "naranja"
+var tipoDeDatoDeLaVariableFruta = typeof(fruta)
+console.log(tipoDeDatoDeLaVariableFruta)
+
+var tipoDeDatoDeLaVariableAuto = typeof(auto)
+console.log(tipoDeDatoDeLaVariableAuto)
+
+var tipoDeDatoDeLaVariableAuto2 = typeof(auto2)
+console.log(tipoDeDatoDeLaVariableAuto2)
+
+var tipoDeDatoDeLaVariableAuto3 = typeof(auto3)
+console.log(tipoDeDatoDeLaVariableAuto3)
